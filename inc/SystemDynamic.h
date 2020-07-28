f3c35cb42fbe6c551e4b8788650c00c49a740697
j900213 <j900213@gmail.com>
2020-07-28 17:55:09 +0200

    Updated until acr cost calculation

--- inc/SystemDynamic.h
+++ inc/SystemDynamic.h
@@ -8,6 +8,12 @@ void PassParameters(SolverInput *InputPtr, DynParameter *ModelParaPtr, EnvFactor
 // Create Line Space [min, max]
 void createLineSpace(real_T *Vector, real_T min, real_T max, uint32_t N);
 
+// Calculate the system dynamics
+void systemDynamics(StateTuple (*Xnext)[NT][NF][NQ], real_T (*ArcCost)[NT][NF][NQ], uint8_t (*InfFlag)[NT][NF][NQ],
+                    real_T const *SpeedVec, real_T const *ForceVec, real_T const *TempVec, real_T const *InletVec,
+                    uint16_t V0_index, uint16_t T0_index, uint16_t N);
+
+/*
 // Calculate the Speed dynamics
 void speedDynamics(uint16_t Nx, uint16_t Nu, real_T (*Xnext)[Nu], real_T (*ArcCost)[Nu], uint8_t (*InfFlag)[Nu],
                    real_T const *StateVec, real_T const *ControlVec, Boundary *BoundaryPtr, uint16_t N,
@@ -19,11 +25,6 @@ void thermalDynamics(uint16_t Nx, uint16_t Nu, real_T (*Xnext)[Nu], real_T (*Arc
                      uint16_t N,
                      uint16_t X0_index);
 
-// Calculate the system dynamics
-void systemDynamics(StateTuple (*Xnext)[NT][NF][NQ], real_T (*ArcCost)[NT][NF][NQ], uint8_t (*InfFlag)[NT][NF][NQ],
-                    real_T const *SpeedVec, real_T const *ForceVec, real_T const *TempVec, real_T const *InletVec,
-                    uint16_t V0_index, uint16_t T0_index, uint16_t N);
-
 // Initialize bridge memory
 void initBridge(Bridge *BridgePtr);
 
@@ -32,3 +33,4 @@ void freeBridge(Bridge *BridgePtr);
 
 // Calculate the Pdc[k] and tDelta[k] which are needed for the thermal solver
 void bridgeConnection(Bridge *BridgePtr, SolverOutput *OutputPtr, real_T V0);
+ */
